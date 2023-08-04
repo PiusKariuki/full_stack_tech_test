fd595064e95748fe1651b660b6f4e23f80e6ea1a
PiusKariuki <piuskariuki19@gmail.com>
2023-08-04 10:09:12 +0300

    Feature: Sending json files under the route /users

--- api/src/app.js
+++ api/src/app.js
@@ -4,11 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const json = require("../data/users")
+const fs = require("fs")
+const path = require("path")
 const app = (0, express_1.default)();
 const PORT = 3001;
+
+
 app.use('/', (req, res) => {
+    console.log('hit')
     res.send('Hello world!');
 });
+
+
 app.listen(PORT, () => {
     console.log('SERVER IS UP ON PORT:', PORT);
 });
