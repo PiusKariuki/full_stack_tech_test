4086f2410960146e3c33c3d4894c84158a9e9e73
PiusKariuki <piuskariuki19@gmail.com>
2023-08-04 10:57:41 +0300

    feature: user modal

--- web/pages/users/index.js
+++ web/pages/users/index.js
@@ -5,12 +5,17 @@ import {useEffect, useState} from "react";
 
 const Users = () => {
     const [users, setUsers] = useState([])
+    const [openModal, setOpenModal] = useState(false)
+    const [formState, setFormState] = useState({
+        name: "",
+        email: "",
+        avatar: ""
+    })
 
     const getAllUsers = () => {
         fetch("http://localhost:3001/users")
             .then(res => res.json())
             .then(data => {
-                console.log('data', data)
                 setUsers(data)
             })
 
@@ -20,10 +25,36 @@ const Users = () => {
         getAllUsers()
     }, []);
 
+    const handleSubmit = (evt) => {
+        evt.preventDefault()
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-RapidAPI-Key': 'your-api-key',
+                'X-RapidAPI-Host': 'jokes-by-api-ninjas.p.rapidapi.com',
+            },
+            body: JSON.stringify(formState)
+        };
+
+        fetch('http://localhost:3001/users', options)
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+    }
+
 
     return (
-        <div className="flex flex-col gap-8 p-10">
-            <p>Users</p>
+        <div className="flex flex-col gap-8 p-10 relative">
+            <div className="flex items-center w-full justify-between">
+                <p>Users</p>
+                <button
+                    onClick={() => setOpenModal(prev => !prev)}
+                    className="bg-blue-500 px-4 py-1 rounded-lg text-white">New User
+                </button>
+            </div>
+
+
             <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
                 {users?.map(item => (
                     <div
@@ -35,6 +66,42 @@ const Users = () => {
                     </div>
                 ))}
             </div>
+
+            {/*    Modal*/}
+            {
+                openModal && (
+                    <form
+                        onSubmit={handleSubmit}
+                        className="fixed z-10 w-full bg-white/90 inset-0 flex flex-col items-center py-10 px-4 gap-8">
+                        <p className="text-lg underline font-semibold text-blue-500">New User Details</p>
+
+                        <div className="flex flex-col gap-2 w-full">
+                            <label htmlFor="name">Name</label>
+                            <input  required={true} id="name" type="text" value={formState.name}
+                                   onChange={evt => setFormState(prev => ({...prev, name: evt.target.value}))}/>
+                        </div>
+
+                        <div className="flex flex-col gap-2 w-full">
+                            <label required={true} htmlFor="email">Email</label>
+                            <input id="email" type="email" value={formState.email}
+                                   onChange={evt => setFormState(prev => ({...prev, email: evt.target.value}))}/>
+                        </div>
+
+                        <div className="flex flex-col gap-2 w-full">
+                            <label required={true} htmlFor="avatar">Avatar url</label>
+                            <input id="avatar" type="text" value={formState.avatar}
+                                   onChange={evt => setFormState(prev => ({...prev, avatar: evt.target.value}))}/>
+                        </div>
+
+                        <button
+                            type="submit"
+                            className="bg-blue-500 px-4 py-1 rounded-lg text-white">New User
+                        </button>
+
+
+                    </form>
+                )
+            }
 
 
         </div>
